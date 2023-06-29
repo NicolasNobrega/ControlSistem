89a5906013930d79eef23f2fe3933d0931290849
Nicolas Alves Nobrega <nicolas1nobrega@gmail.com>
2023-06-29 20:26:58 -0300

    add Dependencies

---
+++ src/main/kotlin/com/example/ControlSistem/ControlSistemApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ControlSistem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControlSistemApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControlSistemApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ControlSistem/ControlSistemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ControlSistem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControlSistemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
